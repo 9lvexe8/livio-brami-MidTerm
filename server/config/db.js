27b8a9c54d29281a247ba92d4941de8fa605a512
Livio <livio.brami98@gmail.com>
2022-10-29 18:59:03 -0400

    some changes

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://127.0.0.1:27017/books"
+  //"URI": "mongodb://127.0.0.1:27017/books"
+  "URI": "mongodb+srv://livioBrami301227775:midtermtest@cluster0.d2d9eet.mongodb.net/books?retryWrites=true&w=majority"
 };
